c4564bcf59879e11832739ee4d2b775bdce82662
AipuAmeh <aipuameh.16@gmail.com>
2025-05-27 17:00:34 -0700

    figured out how to query user inputted data

--- server/views.py
+++ server/views.py
@@ -5,12 +5,13 @@ from pandasql import sqldf
 views = Blueprint('views', __name__)
 
 # rename to specify which chart it will go with
-@views.route('/api/data', methods=['GET'])
+@views.route('/api/data', methods=['GET', 'POST'])
 def get_csv_data():
     data = pd.read_csv('server/data/hirc_pm_report_cleaned.csv')
     if len(data) == 0:
         return jsonify([])
     data = data.where(pd.notnull(data), None)
+    # the query is going to be interactive
     query = """
     SELECT PM_NAME_1 AS project_manager, RSCH_CATG as research_category, COUNT(DISTINCT RSCH_CATG) as count
     FROM data
@@ -19,8 +20,12 @@ def get_csv_data():
     ;
     """
     chart_data = sqldf(query, locals())
-    print(chart_data)
+    # print(chart_data)
     # summary = data.describe(include='all')
     return jsonify(chart_data.to_dict(orient='records'))
+
+@views.route('/some-route')
+def fake():
+    pass
 
     
